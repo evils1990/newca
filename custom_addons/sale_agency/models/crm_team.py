d7173f5fb2261a78a0a57c93d9c6199e01af3f4b
Evils 90 <33918653+evils1990@users.noreply.github.com>
2017-11-30 14:52:58 +0700

    re upgrade order, order line, pricelist, pricelist item

--- custom_addons/sale_agency/models/crm_team.py
+++ custom_addons/sale_agency/models/crm_team.py
@@ -7,3 +7,8 @@ class CrmTeam(models.Model):
     _inherit = 'crm.team'
     crm_type=fields.Selection([('internal','Internal'),('agency','Agency'),('collab','Collaborator')],'Type')
     parent_id=fields.Many2one('crm.team','Parent Team')
+	pricelist_id=fields.Many2one('product.pricelist','Price list')
+
+class ProductPricelist(models.Model):
+    _inherit = 'product.pricelist'
+    team_id=fields.One2many('crm.team','pricelist_id','Sale Team')
